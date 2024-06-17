1dd881932dcc7534c2b9e1aac932bfeeb248ea12
EvelynMorales07 <evemorales.ar@gmail.com>
2024-06-17 17:04:59 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace emoralesT2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
